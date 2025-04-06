1db456e71674233d8bf145241031463bc324a3ad
vv4t <watashidesu6969@gmail.com>
2025-04-06 21:32:56 +1000

    changes

--- workshop/path-tracer/trace.glsl
+++ workshop/path-tracer/trace.glsl
@@ -2,231 +2,132 @@
 #pragma use "../lib/random.glsl"
 #pragma use "../lib/ray_march_with_id.glsl"
 #pragma use "../lib/math.glsl"
+#pragma use "surface.glsl"
+#pragma use "tile_surface.glsl"
 
 #define MAX_BOUNCE 5
 
 out vec4 frag_color;
 in vec2 frag_coord;
 
-struct surface_t {
-  vec3 albedo;
-  vec3 normal;
-  float radiance;
-  float metalness;
-  float roughness;
-};
-
-surface_t surface_solid(vec3 albedo, float metalness, float roughness) {
-  surface_t s;
-  s.albedo = albedo;
-  s.normal = vec3(0.0, 0.0, 1.0);
-  s.metalness = metalness;
-  s.roughness = roughness;
-  s.radiance = 0.0;
-  return s;
-}
-
-surface_t surface_light(vec3 color, float radiance) {
-  surface_t s;
-  s.albedo = color;
-  s.radiance = radiance;
-  return s;
-}
-
-#define TILE 4.0
-
-vec3 get_albedo(vec2 uv)
-{
-  float alpha = 0.0;
-  alpha += min(cos((uv.x * TILE * 4.0 + 1.0) * M_PI) + 0.95, 0.0);
-  alpha += min(cos((uv.y * TILE * 4.0 + 1.0) * M_PI) + 0.95, 0.0);
-  return vec3(1.0 + alpha * 10.0);
-}
-
-float get_roughness(vec2 uv)
-{
-  float alpha = 0.0;
-  
-  if (fract(uv.y * TILE) > 0.5) {
-    alpha = (fract(uv.x * TILE) > 0.5 ? 0.18 : 0.20);
-  } else {
-    alpha = (fract(uv.x * TILE) < 0.5 ? 0.18 : 0.20);
-  }
-  
-  return alpha;
-}
-
-float get_height(vec2 uv)
-{
-  float alpha = 0.0;
-  alpha += min(cos((uv.x * TILE * 4.0 + 1.0) * M_PI) + 0.95, 0.0);
-  alpha += min(cos((uv.y * TILE * 4.0 + 1.0) * M_PI) + 0.95, 0.0);
-  return alpha;
-}
-
-vec3 get_normal(vec2 uv)
-{
-  float u = get_height(uv);
-  float du_x = get_height(uv + vec2(0.001, 0.0)) - u;
-  float du_y = get_height(uv + vec2(0.0, 0.001)) - u;
-  float s = 0.05;
-  return normalize(cross(vec3(s, 0.0, du_x), vec3(0.0, s, du_y)));
-}
-
-surface_t surface_tile(vec3 p) {
-  mat3 p_TBN = map_TBN(p);
-  vec2 uv = (inverse(p_TBN) * p).xy * 0.2;
-  
-  surface_t s;
-  s.albedo = get_albedo(uv);
-  s.normal = get_normal(uv);
-  s.metalness = 0.1;
-  s.roughness = get_roughness(uv);
-  s.radiance = 0.0;
-  return s;
-}
-
-surface_t get_surface(int id, vec3 p) {
-  if (id == 0) {
-    return surface_tile(p);
-  } else if (id == 1) {
-    return surface_light(vec3(0.5, 1.0, 1.0), 5.0);
-  } else if (id == 3) {
-    return surface_light(vec3(1.0, 0.5, 1.0), 2.0);
-  } else if (id == 2) {
-    return surface_solid(vec3(0.4, 0.3, 1.0), 0.4, 0.1);
-  }
-}
-
-/*
-hit_t map(vec3 p) {
-  hit_t s;
-  s.d = MAX_DISTANCE;
-  s.id = 0;
-  
-  s = map_cmp(s, 0, plane(p, vec3(0, 0, -1), -12.0));
-  s = map_cmp(s, 0, plane(p, vec3(0, 0, +1), -12.0));
-  s = map_cmp(s, 1, plane(p, vec3(+1, 0, 0), -18.0));
-  s = map_cmp(s, 0, plane(p, vec3(-1, 0, 0), -12.0));
-  s = map_cmp(s, 0, plane(p, vec3(0, +1, 0), -6.0));
-  s = map_cmp(s, 0, plane(p, vec3(0, -1, 0), -12.0));
-  
-  s = map_cmp(s, 0, cube(p, vec3(-1.0, -3.0, 12.0), vec3(1.0, 6.0, 1.0)));
-  s = map_cmp(s, 0, cube(p, vec3(+7.0, -3.0, 12.0), vec3(1.0, 6.0, 1.0)));
-  s = map_cmp(s, 0, cube(p, vec3( 3.0, 4.0, 12.0), vec3(5.0, 1.0, 1.0)));
-  
-  s = map_cmp(s, 0, cube(p, vec3(-12.0, 0.0, 4.0), vec3(1.0, 12.0, 2.0)));
-  // s = map_cmp(s, 0, cube(p, vec3(-12.0, 0.0, 0.0), vec3(1.0, 12.0, 1.0)));
-  s = map_cmp(s, 0, cube(p, vec3(-12.0, 0.0, -4.0), vec3(1.0, 12.0, 2.0)));
-  
-  s = map_cmp(s, 3, cube(p, vec3( 3.0, -1.0, 12.8), vec3(4.0, 6.0, 1.0)));
-  // s = map_cmp(s, 0, cube(p, vec3( 0.0, 12.0, 1.0), vec3(4.0, 0.5, 4.0)));
-  
-  s = map_cmp(s, 2, sphere(p, vec3(-2.0, -4.0, 4.0), 2.0));
-  
-  return s;
-}
-*/
-
-hit_t map(vec3 p) {
-  hit_t s;
-  s.d = MAX_DISTANCE;
-  
-  s = map_cmp(s, 0, max(plane(p, vec3(0, 0, -1), -12.0), -cube(p, vec3(0.0, -2.0, 64.0), vec3(4.0, 6.0, 128.0))));
-  s = map_cmp(s, 0, plane(p, vec3(0, 0, +1), -6.0));
-  s = map_cmp(s, 0, plane(p, vec3(+1, 0, 0), -6.0));
-  s = map_cmp(s, 0, plane(p, vec3(-1, 0, 0), -6.0));
-  s = map_cmp(s, 0, plane(p, vec3(0, +1, 0), -6.0));
-  s = map_cmp(s, 0, max(plane(p, vec3(0, -1, 0), -6.0), -cube(p, vec3(0.0, 6.0, 7.0), vec3(3.0, 12.0, 3.0))));
-  s = map_cmp(s, 1, cube(p, vec3(0.0, 9.0, 7.0), vec3(3.0, 1.0, 3.0)));
-  
-  // s = map_cmp(s, 3, cube(p, vec3(0.0, -6.0, 7.0), vec3(1.0, 0.125, 4.0)));
-  
-  return s;
-}
-
 uniform sampler2D luminance;
+
+hit_t map(vec3 p);
+surface_t get_surface(vec3 p, int id);
+surface_t surface_light(vec3 color, float radiance);
+surface_t surface_solid(vec3 albedo, float metalness, float roughness);
+vec3 get_sample_seed(vec3 p, int i);
+vec3 randomSpecularRay(vec3 seed, vec3 V, vec3 N, float roughness);
 
 void main() {
   vec2 uv = frag_coord * 2.0 - 1.0;
   uv.x *= resolution.x / resolution.y;
-  vec2 mp = mouse * 2.0 - 1.0;
   
-  vec3 view_pos = vec3(0.0, -2.0, -5.0);
-  mat4 view_mat = mat4(1.0);
+  vec3 view_pos = vec3(-18.0, 3.0, 3.0);
+  mat4 view_mat = mat4(1.0) * rotate_y(1.2) * rotate_x(0.0);
   
   struct {
-    vec3 L;
-    vec3 V;
-    vec3 p;
-    int id;
+    vec3 luminance;
   } stack[MAX_BOUNCE];
   
-  vec3 rand_p = vec3(vec2(rand(time), rand(time * time)) * 2.0 - 1.0, 0.0);
+  vec3 radiance = vec3(0.0);
+  vec3 p = view_pos;
   vec3 rd = normalize(vec3(view_mat * vec4(uv, 1.0, 1.0)));
-  hit_t td = ray_march(view_pos, rd);
   
-  vec3 p = view_pos + rd * td.d;
-  surface_t sf = get_surface(td.id, p);
-  vec3 N = map_normal(p);
-  vec3 radiance = sf.albedo * sf.radiance;
-  
-  if (sf.radiance <= 0.0) {
-    stack[0].id = td.id;
-    stack[0].V = -rd;
-    stack[0].p = p;
+  int i = -1;
+  do {
+    p += rd * 0.0625;
+    hit_t td = ray_march(p, rd);
+    p += rd * td.d;
+    vec3 N = map_normal(p);
+    surface_t sf = get_surface(p, td.id);
     
-    int i = 1;
-    for (; i < MAX_BOUNCE; i++) {
-      vec3 old_rd = rd;
-      rd = randomHemispherePoint(
-        vec3(
-          rand((p.x * (seed.x * 2.0 - 1.0) * float(i)) * 123.0) * 2.0 - 1.0,
-          rand((p.y * (seed.y * 2.0 - 1.0) * float(i)) * 456.0) * 2.0 - 1.0,
-          rand((p.z * (seed.z * 2.0 - 1.0) * float(i)) * 789.0) * 2.0 - 1.0
-        ), N
-      );
-      
-      // rd = normalize(rd + reflect(old_rd, N) * rand((p.x * (seed.z * 2.0 - 1.0) * float(i)) * 789.0) * 4.0);
-      
-      p += rd * 0.0625;
-      td = ray_march(p, rd);
-      p += rd * td.d;
-      sf = get_surface(td.id, p);
-      N = map_normal(p);
+    if (i >= 0) stack[i].luminance *= exp(-td.d * 0.03);
+    
+    if (sf.radiance > 0.0) {
       radiance = sf.albedo * sf.radiance;
+      break;
+    } else {
+      i++;
       
-      stack[i - 1].L = rd;
-      stack[i].V = -rd;
-      stack[i].id = td.id;
-      stack[i].p = p;
+      vec3 sample_seed = get_sample_seed(p, i);
+      bool do_reflect = sample_seed.x > 0.0;
       
-      if (sf.radiance > 0.0) {
-        break;
-      }
-    }
-    
-    float energy = 1.0;
-    for (int j = i - 1; j >= 0; j--) {
-      vec3 p = stack[j].p;
-      mat3 p_TBN = map_TBN(p);
-      vec3 L = stack[j].L;
-      vec3 V = stack[j].V;
-      int id = stack[j].id;
-      sf = get_surface(id, p);
-      vec3 N = p_TBN * sf.normal;
+      vec3 V = -rd;
+      vec3 L = do_reflect ? randomSpecularRay(sample_seed, V, N, sf.roughness) : randomHemispherePoint(sample_seed, N);
+      vec3 H = normalize(L + V);
+      float pdf = do_reflect ? 1.0 : (1.0 / (2.0 * PI));
       
-      float pdf_BRDF = (1.0 / (2.0 * PI));
+      float NdotL = clamp(dot(N, L), 0.0, 1.0);
       vec3 p_BRDF = BRDF(sf.albedo, sf.metalness, sf.roughness, L, V, N);
       
-      radiance = radiance * p_BRDF / pdf_BRDF * dot(N, L) * energy;
-      energy *= 0.8;
+      stack[i].luminance = p_BRDF * NdotL / pdf;
+      rd = L;
     }
-  } else {
-    radiance = radiance / (1.0 / (2.0 * PI));
+  } while (i < MAX_BOUNCE);
+  
+  float energy = 1.0;
+  
+  for (; i >= 0; i--) {
+    radiance = radiance * stack[i].luminance * energy;
+    energy *= 0.9;
   }
   
   vec3 total = texture(luminance, frag_coord).rgb + radiance;
   
   frag_color = vec4(total, 1.0);
+}
+
+vec3 randomSpecularRay(vec3 seed, vec3 V, vec3 N, float roughness) {
+  return normalize(randomSpherePoint(seed) + reflect(-V, N) * exp((1.0 - roughness) * 3.0));
+}
+
+surface_t get_surface(vec3 p, int id) {
+  if (id == 0) {
+    return surface_tile(p);
+  } else if (id == 1) {
+    return surface_light(vec3(0.8, 1.0, 1.0), 30.0);
+  } else if (id == 2) {
+    return surface_solid(vec3(1.0, 0.5, 1.0), 0.9, 0.4);
+  } else if (id == 3) {
+    return surface_solid(vec3(0.5, 0.5, 1.0), 0.1, 0.9);
+  }
+}
+
+hit_t map(vec3 p) {
+  hit_t s;
+  s.d = MAX_DISTANCE;
+  s.id = 0;
+  
+  // walls
+  s = map_add(s, 0, plane(p, vec3(0, 0, -1), -20.0));
+  s = map_add(s, 0, plane(p, vec3(0, 0, +1), 0.0));
+  s = map_add(s, 0, plane(p, vec3(+1, 0, 0), -32.0));
+  s = map_add(s, 0, plane(p, vec3(-1, 0, 0), 0.0));
+  s = map_add(s, 0, plane(p, vec3(0, +1, 0), 0.0));
+  s = map_add(s, 0, plane(p, vec3(0, -1, 0), -8.0));
+  
+  // light
+  s = map_add(s, 1, cube(p, vec3(-3.0, 8.0, 13.0), vec3(1.0, 0.125, 4.0)));
+  
+  // corridor
+  s = map_add(s, 0, cube(p, vec3(-9.0, 4.0, 4.0), vec3(1.0, 4.0, 1.0)));
+  s = map_add(s, 0, cube(p, vec3(-9.0, 4.0, 10.0), vec3(1.0, 4.0, 1.0)));
+  s = map_add(s, 0, cube(p, vec3(-9.0, 4.0, 16.0), vec3(1.0, 4.0, 1.0)));
+  // s = map_add(s, 0, cube(p, vec3(-9.0, 7.0, 10.0), vec3(4.0, 1.0, 6.0)));
+  
+  // doors
+  s = map_sub(s, 0, cube(p, vec3(3.0, 3.0, 7.0), vec3(6.0, 3.0, 2.0)));
+  s = map_sub(s, 0, cube(p, vec3(3.0, 3.0, 13.0), vec3(6.0, 3.0, 2.0)));
+  
+  // sphere
+  s = map_add(s, 2, sphere(p, vec3(-6.0, 1.0, 7.0), 1.0));
+  s = map_add(s, 3, sphere(p, vec3(-0.0, 1.5, 7.0), 1.5));
+  
+  return s;
+}
+
+vec3 get_sample_seed(vec3 p, int i) {
+  vec3 v = p * 2404.4324 * seed + seed * 15043.4234 + rand(float(i + 1)) * 13242.4352;
+  return fract(sin(v) * 43758.5453123) * 2.0 - 1.0;
 }
