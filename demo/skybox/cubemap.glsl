55e6571e99af88d9da9cb94115c97a500a7afe2b
vv4t <watashidesu6969@gmail.com>
2025-05-17 03:12:22 +1000

    input to cubemap

--- demo/skybox/cubemap.glsl
+++ demo/skybox/cubemap.glsl
@@ -1,8 +1,14 @@
+#pragma use "../util/math.glsl"
+
 out vec4 fragColor;
 
 uniform samplerCube sky;
 
 void main() {
-  vec2 uv = gl_FragCoord.xy / iResolution.xy;
-  fragColor = texture(sky, vec3(uv, 1));
+  vec2 uv = (gl_FragCoord.xy / iResolution.xy * 2.0 - 1.0) * vec2(iResolution.x / iResolution.y, 1.0);
+  
+  vec2 viewRotation = (iMouse.xy * 2.0 - 1.0) * 4.0;
+  mat4 viewMatrix = mat4(1.0) * rotateY(viewRotation.x) * rotateX(viewRotation.y);
+  
+  fragColor = texture(sky, (viewMatrix * vec4(uv, 1.0, 1.0)).xyz);
 }
